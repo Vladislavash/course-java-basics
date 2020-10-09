967d08ddf440784e07f2b9d373ea151020ddf5d7
Vladislavash <71904943+Vladislavash@users.noreply.github.com>
2020-10-10 00:01:41 +0300

    Add files via upload

---
+++ src/main/java/com/rakovets/course/javabasics/practice/loops/Task05.java
@@ -0,0 +1,44 @@
+package com.rakovets.course.javabasics.practice.loops;
+
+import java.util.Scanner;
+
+/**
+ * Разработать программу для игрового движка:
+ * Спрогнозировать через какое время party(team) игроков убьет RaidBoss и получит вознаграждение. Известно:
+ *
+ * @param healthPoints               количество HP RaidBoss
+ * @param regenerationPercentPerHour регенерация HP RaidBoss (%/hour)
+ * @param averageDamagePerHour       средний урон команды игроков по Raid Boss (HP/hour)
+ * @author Dmitry Rakovets
+ * @version 1.0
+ * @return время для убийства RaidBoss (когда party не справляется за 24 часа, то вывести -1)
+ */
+public class Task05 {
+    public static void main(String[] args) {
+        // Ввод данных осуществляется в Console, для проверки различных вариантов входных параметров
+        Scanner scanner = new Scanner(System.in);
+
+        // Код необходимый для тестирования, не изменять
+        int healthPoints = (args.length != 3 ? scanner.nextInt() : Integer.parseInt(args[0]));
+        double regenerationPercentFromCurrentHealth = (args.length != 3 ? scanner.nextDouble() : Double.parseDouble(args[1]));
+        int averageDamagePerHour = (args.length != 3 ? scanner.nextInt() : Integer.parseInt(args[2]));
+        int hour = 0;
+        double healthPoints1 = (double)healthPoints;
+        double averageDamagePerHour1 = (double)averageDamagePerHour;
+        while (healthPoints1 > 0) {
+            healthPoints1 = healthPoints1 * (1 + regenerationPercentFromCurrentHealth / 100) - averageDamagePerHour1;
+            hour++;
+            if (hour > 24) {
+                break;
+            }
+        }
+        if (healthPoints1 > 0) {
+            System.out.println("-1");
+        } else {
+            System.out.println(hour);
+        }
+    }
+}
+        //TODO
+        // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
+        // Для проверки решения необходимо запустить @Test для данного class (в директории test)
