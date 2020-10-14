c804bf4a9d87a00ce3ec9949f575100b296705b7
Vladislava Shugalo <opop.pooop.00@mail.ru>
2020-10-14 23:19:59 +0300

    feat: add tasks for theme 'oop-classes-and-objects'

--- src/main/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/Atm.java
+++ src/main/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/Atm.java
@@ -1,25 +1,42 @@
 package com.rakovets.course.javabasics.practice.oop.classesandobjects;
 
 public class Atm {
-    public static void main(String[] args) {
-        int numberBanknotes100;
-        int numberBanknotes50;
-        int numberBanknotes20;
-        Atm (int numberBanknotes100, int numberBanknotes50, int numberBanknotes20) {
-            this.numberBanknotes100 = numberBanknotes100;
-            this.numberBanknotes50 = numberBanknotes50;
-            this.numberBanknotes20 = numberBanknotes20;
+    private int numberBanknotes100;
+    private int numberBanknotes50;
+    private int numberBanknotes20;
+
+    Atm(int numberBanknotes100, int numberBanknotes50, int numberBanknotes20) {
+        this.numberBanknotes100 = numberBanknotes100;
+        this.numberBanknotes50 = numberBanknotes50;
+        this.numberBanknotes20 = numberBanknotes20;
+    }
+
+    int addBanknotes100(int number) {
+        return this.numberBanknotes100 = number;
+    }
+
+    int addBanknotes50(int number) {
+        return this.numberBanknotes50 = number;
+    }
+
+    int addBanknotes20(int number) {
+        return this.numberBanknotes20 = number;
+    }
+
+    void isPossibleIssue(int amount) {
+        for (int i100 = 0; i100 <= this.numberBanknotes100; i100++) {
+            for (int i50 = 0; i50 <= this.numberBanknotes50; i50++) {
+                for (int i20 = 0; i20 <= this.numberBanknotes20; i20++) {
+                    if (i100 + i50 + i20 == amount) {
+                        System.out.println(true);
+                    } else {
+                        System.out.println(false);
+                    }
+                    break;
+                }
+                break;
+            }
+            break;
         }
-        int getX() {
-            return this.x;
-        }
-        int setX(int x) {
-            this.x = x;
-        }
-        int getY() {
-            return this.y;
-        }
-        int setY(int y) {
-            this.y = y;
-        }
+    }
 }
